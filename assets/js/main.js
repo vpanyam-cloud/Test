d8e09cd0d9e209411eec930bf7fb19db21a615b4
vpanyam-cloud <82635283+vpanyam-cloud@users.noreply.github.com>
2021-04-16 11:27:02 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@
 	(n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
   var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
   var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-DT38BLOGLNV8-2");
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-LFPJVJCZ9U5B-2");
 
 
 
